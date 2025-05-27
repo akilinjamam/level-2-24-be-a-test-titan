8ffddb8791c9f0190dafe3e610e4465daba93261
akilinjamam <akilinjamam@gmail.com>
2025-05-27 06:57:21 +0600

    updated

--- math.test.js
+++ math.test.js
@@ -1,51 +1,54 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { add } from './math';
 
-it('should add numbers', () => {
-    // arrange
-    const numbers = [1, 2, 3, 4, 5];
-    const expectedResult = numbers.reduce((acc, sum) => acc + sum);
+describe('add()', () => {
+    it('should add numbers', () => {
+        // arrange
+        const numbers = [1, 2, 3, 4, 5];
+        const expectedResult = numbers.reduce((acc, sum) => acc + sum);
 
-    // action
-    const result = add(numbers);
+        // action
+        const result = add(numbers);
 
-    // assertion 
-    expect(result).toBe(expectedResult);
+        // assertion 
+        expect(result).toBe(expectedResult);
+    })
+
+
+    it('it should provide NaN if at least one invalid number is provided', () => {
+        // arrange
+        const numbers = [1, 2, 'invalid', 4, 5];
+
+
+        // action
+        const result = add(numbers);
+
+        // assertion 
+        expect(result).toBeNaN();
+    })
+
+
+    it('it should provide correct sum if an array numeric string is provided', () => {
+        // arrange
+        const numbers = ['1', '2', '3', '4', '7'];
+        const expectedResult = numbers.reduce((acc, sum) => +acc + +sum);
+
+        // action
+        const result = add(numbers);
+
+        // assertion 
+        expect(result).toBe(expectedResult);
+    })
+    it('it should through an error if no argument passed', () => {
+
+        const resultFn = () => add();
+
+        expect(resultFn).toThrow();
+    })
+
+    // it('should throw an error if multiple arguments are provided', () => {
+    //     const resultFn = () => add(1, 2, 3);
+    //     expect(resultFn).toThrow(/is not iterable/i);
+    // });
+
 })
-
-
-it('it should provide NaN if at least one invalid number is provided', () => {
-    // arrange
-    const numbers = [1, 2, 'invalid', 4, 5];
-
-
-    // action
-    const result = add(numbers);
-
-    // assertion 
-    expect(result).toBeNaN();
-})
-
-
-it('it should provide correct sum if an array numeric string is provided', () => {
-    // arrange
-    const numbers = ['1', '2', '3', '4', '7'];
-    const expectedResult = numbers.reduce((acc, sum) => +acc + +sum);
-
-    // action
-    const result = add(numbers);
-
-    // assertion 
-    expect(result).toBe(expectedResult);
-})
-it('it should through an error if no argument passed', () => {
-
-    const resultFn = () => add();
-
-    expect(resultFn).toThrow();
-})
-
-// it('should throw an error if multiple arguments are provided', () => {
-//     const resultFn = () => add(1, 2, 3);
-//     expect(resultFn).toThrow(/is not iterable/i);
-// });
--- validation.test.js
+++ validation.test.js
@@ -1,39 +1,45 @@
-import { expect, it } from "vitest";
+import { expect, it, describe } from "vitest";
 import { validateArrayNotEmpty, validationEmail } from "./validation";
 
-it('should validate the input', () => {
-    const email = 'example@gmail.com';
-    const resultFn = () => validationEmail(email);
-    expect(resultFn).not.toThrow();
-})
-it('throw an error for a empty string pass', () => {
-    const email = '';
-    const resultFn = () => validationEmail(email);
-    expect(resultFn).toThrow();
-})
-it('throw an error for null email address', () => {
+describe('validationEmail()', () => {
+    it('should validate the input', () => {
+        const email = 'example@gmail.com';
+        const resultFn = () => validationEmail(email);
+        expect(resultFn).not.toThrow();
+    })
+    it('throw an error for a empty string pass', () => {
+        const email = '';
+        const resultFn = () => validationEmail(email);
+        expect(resultFn).toThrow();
+    })
+    it('throw an error for null email address', () => {
 
-    const resultFn = () => validationEmail(null);
-    expect(resultFn).toThrow('Invalid email address');
+        const resultFn = () => validationEmail(null);
+        expect(resultFn).toThrow('Invalid email address');
+    })
+    it('throw an error for undefine email address', () => {
+
+        const resultFn = () => validationEmail(undefined);
+        expect(resultFn).toThrow('Invalid email address');
+    })
 })
-it('throw an error for undefine email address', () => {
 
-    const resultFn = () => validationEmail(undefined);
-    expect(resultFn).toThrow('Invalid email address');
-})
 
-it('should validate non empty array', () => {
 
-    const resultFn = () => validateArrayNotEmpty([1, 2, 3]);
-    expect(resultFn).not.toThrow();
-})
-it('through an error for a undefined input ', () => {
+describe('validateArrayNotEmpty()', () => {
+    it('should validate non empty array', () => {
 
-    const resultFn = () => validateArrayNotEmpty(undefined);
-    expect(resultFn).toThrow();
-})
-it('through an error for a null input ', () => {
+        const resultFn = () => validateArrayNotEmpty([1, 2, 3]);
+        expect(resultFn).not.toThrow();
+    })
+    it('through an error for a undefined input ', () => {
 
-    const resultFn = () => validateArrayNotEmpty(null);
-    expect(resultFn).toThrow();
+        const resultFn = () => validateArrayNotEmpty(undefined);
+        expect(resultFn).toThrow();
+    })
+    it('through an error for a null input ', () => {
+
+        const resultFn = () => validateArrayNotEmpty(null);
+        expect(resultFn).toThrow();
+    })
 })
